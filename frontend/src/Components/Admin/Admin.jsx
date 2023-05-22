4c67a17603cdc61ccdc13fcf668475d0ef7f317f
Yassinos-Coder <37705952+Yassinos-coder@users.noreply.github.com>
2023-05-22 17:52:37 +0100

    Job Finished

--- frontend/src/Components/Admin/Admin.jsx
+++ frontend/src/Components/Admin/Admin.jsx
@@ -3,8 +3,37 @@ import React from "react";
 import rectangle from "../../assets/imgs/rectangle.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { useEffect } from "react";
+import { GetAllOrders } from "../../redux/OrdersReducer";
 
 const Admin = () => {
+  const dispatch = useDispatch();
+  const Orders = useSelector((state) => state.OrdersReducer.Orders);
+  const [currentPage, setCurrentPage] = useState(1);
+  const orderPerPage = 6;
+  const lastIndex = currentPage * orderPerPage;
+  const firstIndex = lastIndex - orderPerPage;
+  const orderToDisplay = Orders.slice(firstIndex, lastIndex);
+  const pageNumber = Math.ceil(Orders.length / orderPerPage);
+  const numbers = [...Array(pageNumber + 1).keys()].slice(1);
+
+  useEffect(() => {
+    dispatch(GetAllOrders());
+  }, []);
+
+  const handlePrevPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage !== lastIndex) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
   return (
     <>
       <div className="admin">
@@ -13,7 +42,7 @@ const Admin = () => {
           <div className="data">
             <div className="rectData">
               <div className="image-container">
-                <img src={rectangle} alt="" srcset="" />
+                <img src={rectangle} alt="" />
               </div>{" "}
               <div className="imgInfo">
                 <p className="numbers">30,000</p>
@@ -22,7 +51,7 @@ const Admin = () => {
             </div>
             <div className="rectData">
               <div className="image-container">
-                <img src={rectangle} alt="" srcset="" />
+                <img src={rectangle} alt="" />
               </div>
               <div className="imgInfo">
                 <p className="numbers">250</p>
@@ -31,7 +60,7 @@ const Admin = () => {
             </div>
             <div className="rectData">
               <div className="image-container">
-                <img src={rectangle} alt="" srcset="" />
+                <img src={rectangle} alt="" />
               </div>{" "}
               <div className="imgInfo">
                 <p className="numbers">$80,000</p>
@@ -63,12 +92,85 @@ const Admin = () => {
                 </div>
                 <div className="filter">
                   <button type="submit" className="buttonFilter">
-                    <FontAwesomeIcon style={{paddingRight: '7px'}} icon={faFilter} />
+                    <FontAwesomeIcon
+                      style={{ paddingRight: "7px" }}
+                      icon={faFilter}
+                    />
                     Filter
                   </button>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="dataTable">
+            <table className="table">
+              <thead className="table-thead">
+                <tr className="table-thead">
+                  <th>Name</th>
+                  <th>Phone</th>
+                  <th>Status</th>
+                  <th>Unity Price</th>
+                  <th>Quantity</th>
+                  <th>Total Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orderToDisplay.map((order, index) => (
+                  <tr key={index}>
+                    <td>{order.orderer_name.split(" ")[0]}</td>
+                    <td>0{order.orderer_phonenumber}</td>
+                    <td>
+                      <span
+                        className={`orderStatus ${
+                          order.order_status === "Verified"
+                            ? "verifiedStatus"
+                            : order.order_status === "Pending"
+                            ? "pendingStatus"
+                            : ""
+                        }`}
+                      >
+                        {order.order_status}
+                      </span>
+                    </td>
+                    <td>${order.product_price}</td>
+                    <td>{order.order_qty}</td>
+                    <td>${order.order_qty * order.product_price}</td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="6">
+                    <div className="tableActions">
+                      <button
+                        className="tableActionBTN"
+                        onClick={handlePrevPage}
+                      >
+                        Previous Page
+                      </button>
+                      <ul className="tableActionUL">
+                        {numbers.map((number, index) => (
+                          <li
+                            key={index}
+                            onClick={() => {
+                              setCurrentPage(number);
+                            }}
+                          >
+                            <div className={`numberPage ${currentPage === number ? 'active': ''}`}>{number}</div>
+                          </li>
+                        ))}
+                      </ul>
+                      <button
+                        className="tableActionBTN"
+                        onClick={handleNextPage}
+                      >
+                        Next Page
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
           </div>
         </div>
       </div>
